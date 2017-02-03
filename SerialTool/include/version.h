c055da8310947da99611a6514800e80e427fc3e5
Le-Seul <2269610337@qq.com>
2017-02-03 19:08:45 +0800

    第8次提交

--- SerialTool/include/version.h
+++ SerialTool/include/version.h
@@ -9,7 +9,7 @@
 #define _STR_(s)           #s
 #define __STR(s)          _STR_(s)
 
-#define BUILD_VERSION     _STR_(6a38M)
+#define BUILD_VERSION     _STR_(7a3c)
 #define SOFTWARE_VERSION  __STR(MAIN_VERSION)
 
 #endif
